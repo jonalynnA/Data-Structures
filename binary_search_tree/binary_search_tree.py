a611c3cd0c8f034e10911764310c43d8f91783d3
jonalynn masters <jonalynna.masters@gmail.com>
2020-04-23 14:41:55 -0700

    LRU Complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -10,40 +10,104 @@ class BinarySearchTree:
         self.left = None
         self.right = None
 
-    # Insert the given value into the tree
+    # Insert the given value into tree
     def insert(self, value):
-        pass
+        # if value is less than self.value
+        if value < self.value:
+            # if left is none
+            if self.left is None:
+                # set left to be node
+                self.left = BinarySearchTree(value)
+                # if not set left node this value
+            else:
+                self.left.insert(value)
+
+        else:
+          # do same for right side
+            if value >= self.value:
+
+                if self.right is None:
+
+                    self.right = BinarySearchTree(value)
+        # if it has a node, call self.right.insert with this value
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value:
+            return True
+
+        elif self.right and self.value < target:
+            return self.right.contains(target)
+
+        elif self.left and self.value > target:
+            return self.left.contains(target)
+
+        else:
+            return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right != None:
+            return self.right.get_max()
+        else:
+            return self.value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.right is None:
+            pass
+        else:
+            self.right.for_each(cb)  # pass cb
+
+        if self.left is None:
+            pass
+        else:  # same for left
+            self.left.for_each(cb)
+        cb(self.value)
 
     # DAY 2 Project -----------------------
-
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
+
     def in_order_print(self, node):
-        pass
+        if node:  # If a node is passed in recursively, run code block, otherwise end
+            self.in_order_print(node.left)  # pass in left
+            # print(node.value)  # print current value
+            self.in_order_print(node.right)  # pass in right
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        queue = Queue()  # instantiate Queue Class
+        queue.enqueue(self)  # add root to (end of) queue
+        while queue.size() > 0:  # While there is something in queue
+            current_node = queue.dequeue()  # grab node from front of queue
+            # Print current_node value
+
+            if current_node.left:  # If left
+                queue.enqueue(current_node.left)  # Add left to end of queue
+
+            if current_node.right:  # If right
+                queue.enqueue(current_node.right)  # Add right to end of queue
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()  # instantiate Stack Class
+        stack.push(self)  # add root to stack
+        while stack.size() > 0:  # While there is something in stack
+            current_node = stack.pop()  # Grab (top) node from stack
+            # Print current_node value
+            # print(current_node.value)
+            if current_node.left:  # If left
+                stack.push(current_node.left)  # Add left to stack
+            if current_node.right:  # If right
+                stack.push(current_node.right)  # Add right to stack
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
---
+++ binary_search_tree/dll_queue.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+# NOTE: Think of queue like music queue or store line or grabbing milk...first in last out
+# NOTE: Think of stack like a stack of plates, last one on stack first one off stack
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        # If you didn't have a .prev on your tail node, you would have to start
+        # at the beginning of the list so O(n) to find the tail versus being able to traverse
+        # backwards and have complexity of O(1). If you did not have to queue with enque and deque
+        # then you could use LL
+
+        self.storage = DoublyLinkedList()
+
+    def enqueue(self, value):
+        # add item to the list
+        # (inherently adding something to a list puts it at the end) (First in last out)
+        # end of list = (the tail)(the bottom)(the right)
+        # function add_to_tail from DoublyLinkedList class does this
+        # list size is important, remember to update list size with each function
+        # enqueue adds one to the list so size will increase by 1
+        self.size += 1
+        return self.storage.add_to_tail(value)
+
+    def dequeue(self):
+        # remove item from the list(first in last out)
+        # removing from the list or in a queue means removing from the front of the list
+        # front of list = (the head)(the top)(the front)(the left)
+        # function remove_from_head from DoublyLinkedList class does this
+        # list size is important, remember to update list size
+        # dequeue removes a value from the list so size decreases by 1
+        if self.size == 0:  # could also say if < 1
+            return None
+        else:
+            self.size -= 1
+        return self.storage.remove_from_head()
+
+    def len(self):
+        return self.size
